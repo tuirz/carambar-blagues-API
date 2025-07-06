f98e49330c6d3496f827ab8c9a67ed10a36c9a0f
Luigi SOUFAR <basses_bouleau.3n@icloud.com>
2025-07-06 16:23:26 +0200

    fix seeders complet

--- src/seeders/blaguesSeeder.js
+++ src/seeders/blaguesSeeder.js
@@ -12,6 +12,34 @@ const blagues = [
   {
     question: "Quel est l'animal le plus heureux ?",
     reponse: "Le hibou, parce que sa femme est chouette."
+  },
+  {
+    question: "Pourquoi le football c'est rigolo ?",
+    reponse: "Parce que Thierry en rit"
+  },
+  {
+    question: "Quel est le sport le plus fruité ?",
+    reponse: "La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes."
+  },
+  {
+    question: "Que se fait un Schtroumpf quand il tombe ?",
+    reponse: "Un Bleu"
+  },
+  {
+    question: "Quel est le comble pour un marin ?",
+    reponse: "Avoir le nez qui coule"
+  },
+  {
+    question: "Qu'est ce que les enfants usent le plus à l'école ?",
+    reponse: "Le professeur"
+  },
+  {
+    question: "Quel est le sport le plus silencieux ?",
+    reponse: "Le para-chuuuut"
+  },
+  {
+    question: "Quel est le comble pour un joueur de bowling ?",
+    reponse: "C'est de perdre la boule"
   }
 ];
 
